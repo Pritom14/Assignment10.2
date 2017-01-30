64b1a98ca22a84e3109e3a7999928d3a06cc9637
Pritom14 <pritommazumdar1995@gmail.com>
2017-01-30 11:13:16 +0530

    Assignment10.2

---
+++ TenthAssignment/app/src/main/java/com/example/shaloin/tenthassignmentb/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.shaloin.tenthassignmentb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList versions;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+    }
+    private void initViews(){
+        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.card_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        versions = new ArrayList<>();
+        versions.add("CupCake");
+        versions.add("Donut");
+        versions.add("Eclairs");
+        versions.add("Froyo");
+        versions.add("GingerBread");
+        versions.add("HoneyComb");
+        versions.add("Icecream sandwich");
+        versions.add("JellyBean");
+        versions.add("KitKat");
+        versions.add("Lollipop");
+        versions.add("MarshMallow");
+        versions.add("Nougat");
+        RecyclerView.Adapter adapter = new DataAdapter(versions);
+        recyclerView.setAdapter(adapter);
+
+        recyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
+            GestureDetector gestureDetector = new GestureDetector(getApplicationContext(), new GestureDetector.SimpleOnGestureListener() {
+
+                @Override public boolean onSingleTapUp(MotionEvent e) {
+                    return true;
+                }
+
+            });
+            @Override
+            public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+
+                View child = rv.findChildViewUnder(e.getX(), e.getY());
+                if(child != null && gestureDetector.onTouchEvent(e)) {
+                    int position = rv.getChildAdapterPosition(child);
+                    Toast.makeText(MainActivity.this, (int) versions.get(position), Toast.LENGTH_SHORT).show();
+                }
+
+                return false;
+            }
+
+            @Override
+            public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+            }
+
+            @Override
+            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+            }
+        });
+    }
+}
